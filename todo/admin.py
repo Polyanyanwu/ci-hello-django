b85d3b0f3d3d45c64da21c706d4fbb61ac465ee0
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-03-14 13:14:31 +0000

    Add render template

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,9 @@
+""" Admin for models """
+
 from django.contrib import admin
+from .models import Item
+
 
 # Register your models here.
+
+admin.site.register(Item)
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render
+from .models import Item
 # Create your views here.
 
-def say_hello(request):
-    return HttpResponse("Hello Poly!")
+
+def get_todo_list(request):
+    """get todo list """
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
