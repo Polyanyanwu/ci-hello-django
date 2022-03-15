c7440837e5ad4bd72141ed924c48110a9b85380f
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-03-15 13:11:31 +0000

    Add test files

---
+++ todo/test_views.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+
+class TestDjango(TestCase):
+    """ create tests here """
+    def test_this_thing_works(self):
+        self.assertEqual(1, 1)
+
+
