66679122c181eb1ee3ff9238db122382693d2668
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-03-15 15:45:36 +0000

    Add test models

--- todo/models.py
+++ todo/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Item(models.Model):
     """ Item table """
     name = models.CharField(max_length=50, null=False, blank=False)
-    done = models.BooleanField(null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
 
     def __str__(self):
         return self.name
